09bdd2596c4bf25d580ef1328c7c04f68b4af67b
Jean-Francois BAFFIER <jeanfrancois.baffier@gmail.com>
2015-09-07 17:28:19 +0900

    Test Arc in links.jl test 2

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using NetworkFlows
 using Base.Test
 
 ## Test link.jl ##
+import NetworkFlows.zero_to_one!
 @test zero_to_one!([(0,1),(1,2)]) == [(1,2),(2,3)]
 @test zero_to_one!([(3,1),(1,2)]) != [(3,1),(1,2)]
 @test zero_to_one!([(0,1,1.),(1,2,2.)]) == [(1,2,1.),(2,3,2.)]
