c006205a608b09c62c0e180afea4526c585ef03f
Jean-Francois BAFFIER <jeanfrancois.baffier@gmail.com>
2015-09-07 16:45:57 +0900

    Tests for zero_to_one!

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using NetworkFlows
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+## Test link.jl ##
+@test zero_to_one!([(0,1),(1,2)]) == [(1,2),(2,3)]
+@test zero_to_one!([(3,1),(1,2)]) == [(3,1),(1,2)]
+@test zero_to_one!([(0,1,1.),(1,2,2.)]) == [(1,2,1.),(2,3,2.)]
+@test zero_to_one!([(3,1,0.),(1,2,-2.1)]) == [(3,1,0.),(1,2,-2.1)]
