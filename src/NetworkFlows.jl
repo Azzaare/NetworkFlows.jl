17172b80c9a539d8790911b00d05ffff4b8e58a1
Jean-Francois BAFFIER <jeanfrancois.baffier@gmail.com>
2015-09-07 17:23:35 +0900

    Test Arc in links.jl

--- src/NetworkFlows.jl
+++ src/NetworkFlows.jl
@@ -1,7 +1,5 @@
 module NetworkFlows
 
-export zero_to_one!
-
 include("links.jl")
 include("network.jl")
 include("io.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,12 @@ using Base.Test
 @test zero_to_one!([(0,1,1.),(1,2,2.)]) == [(1,2,1.),(2,3,2.)]
 @test zero_to_one!([(3,1,0.),(1,2,-2.1)]) != [(3,1,0.),(1,2,-2.1)]
 
+import NetworkFlows.Arc, NetworkFlows.ghostArc, NetworkFlows.simpleArc
+@test Arc(0,1,0.) == ghostArc(1)
+@test Arc(0,2,1.) == ghostArc(2)
+
 ## Test network.jl
+
 
 ## Test io.jl
 
