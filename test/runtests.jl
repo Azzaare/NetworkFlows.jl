ea53db999900ea707c128e4b0382c893617fbb78
Jean-Francois BAFFIER <jeanfrancois.baffier@gmail.com>
2015-09-08 09:14:54 +0900

    Test Arc in links.jl test 5

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,10 +9,12 @@ import NetworkFlows.zero_to_one!
 @test zero_to_one!([(3,1,0.),(1,2,-2.1)]) != [(3,1,0.),(1,2,-2.1)]
 
 import NetworkFlows.Arc, NetworkFlows.ghostArc, NetworkFlows.simpleArc
-@test Arc(0,1,0.)
-@test Arc(0,2,1.)
-@test ghostArc(1)
-@test simpleArc(2)
+@test Arc(0,1,0.).sym == ghostArc(1).sym
+@test Arc(0,1,0.).head == ghostArc(1).head
+@test Arc(0,1,0.).cap == ghostArc(1).cap
+@test Arc(0,2,1.).sym == simpleArc(2).sym
+@test Arc(0,2,1.).head == simpleArc(2).head
+@test Arc(0,2,1.).cap == simpleArc(2).cap
 
 ## Test network.jl
 
