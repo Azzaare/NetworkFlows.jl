f15c16193355fa6168c8d2bba722c1446e37e4a7
Jean-Francois BAFFIER <jeanfrancois.baffier@gmail.com>
2015-09-08 09:32:54 +0900

    Test for network.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,7 +17,13 @@ import NetworkFlows.Arc, NetworkFlows.ghostArc, NetworkFlows.simpleArc
 @test Arc(0,2,1.).cap == simpleArc(2).cap
 
 ## Test network.jl
-
+g1 = Network(1,2,[1,2,2],[Arc(0,2,1.)])
+g2 = Network(2,1,[1,2,2],[simpleArc(2)])
+@test g1.source == g2.target
+@test g2.source == g1.target
+@test g1.tails == g2.tails
+@test length(g1.links) == length(g2.links)
+@test findArc(g1,1,2) == findArc(g2,1,2)
 
 ## Test io.jl
 
