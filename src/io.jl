c6a577bc0cfc18e8fa5bb74d929529979497849c
Jean-Francois BAFFIER <jeanfrancois.baffier@gmail.com>
2015-10-13 11:26:52 +0900

    Update String to AbstractString for Julia v0.4 release

--- src/io.jl
+++ src/io.jl
@@ -76,7 +76,7 @@ function Network(links::Vector{SimpleLink}, directed::Bool, s::Int, t::Int)
 end
 
 # DIMACS format input
-function orderedDIMACS(fname::String, foutput = "")
+function orderedDIMACS(fname::AbstractString, foutput = "")
   # Init Strings
   comments = Vector{String}()
   links = Vector{Link}()
@@ -141,7 +141,7 @@ function orderedDIMACS(fname::String, foutput = "")
   end
 end
 
-function dimacsNetwork(fname::String)
+function dimacsNetwork(fname::AbstractString)
   links = Vector{Link}()
   source = 0
   sink = 0
@@ -168,7 +168,7 @@ function dimacsNetwork(fname::String)
   Network(links, directed, source, sink)
 end
 
-function csvNetwork(fname::String, source::Int, sink::Int)
+function csvNetwork(fname::AbstractString, source::Int, sink::Int)
   links = Vector{Link}()
   directed = true
 
@@ -179,7 +179,7 @@ function csvNetwork(fname::String, source::Int, sink::Int)
   Network(links,directed,source,sink)
 end
 
-function Network(fname::String,format::Symbol, st = (1,2) )
+function Network(fname::AbstractString,format::Symbol, st = (1,2) )
   if format == :DIMACS
     dimacsNetwork(fname)
   elseif format == :CSV
